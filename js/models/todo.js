7b3f5e77d8b6885982eaf9cf07abb3c8bb269ec9
tannn <tannn@miichisoft.com>
2019-08-08 17:16:48 +0700

    update  code

---
+++ js/models/todo.js
@@ -0,0 +1,13 @@
+var app = app || {};
+
+app.Todo = Backbone.Model.extend({
+    defaults: {
+        title: '',
+        completed: false
+    },
+    toogle: function () {
+        this.save({
+            completed: !this.get('completed')
+        });
+    }
+})
